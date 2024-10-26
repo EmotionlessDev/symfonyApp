a920f270b3ca3d9136806c02798ebf8ec98a7425
EmotionlessDev <emotionlesscode@gmail.com>
2024-10-27 04:01:35 +1000

    added Stock entity & mv docker-compose

---
+++ src/Entity/Stock.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StockRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StockRepository::class)]
+class Stock
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ticket = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTicket(): ?string
+    {
+        return $this->ticket;
+    }
+
+    public function setTicket(string $ticket): static
+    {
+        $this->ticket = $ticket;
+
+        return $this;
+    }
+}
