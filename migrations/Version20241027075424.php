ea83b8b2014a06b79dd281461e4280f1efbe31fa
EmotionlessDev <emotionlesscode@gmail.com>
2024-10-27 17:55:56 +1000

    mv docker-compose & fix migrations

--- migrations/Version20241027075424.php
+++ migrations/Version20241027075424.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241026063724 extends AbstractMigration
+final class Version20241027075424 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20241026063724 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE stock ADD name VARCHAR(255) NOT NULL, ADD ticket VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE hello (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stock (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, ticket VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE stock DROP name, DROP ticket');
+        $this->addSql('DROP TABLE hello');
+        $this->addSql('DROP TABLE stock');
     }
 }
